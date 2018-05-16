db6ec9fcde9a226192b13abef36f3931b4488ea8
xiongxiaotao <280629781@qq.com>
2018-05-16 18:26:17 +0800

    1

---
+++ src/core/context.js
@@ -0,0 +1,6 @@
+export default class {
+    constructor () {
+        this.name = '';
+    }
+    render () { }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+import Context from './core/context';
+
 export default {
     init (config = {
         type: 'bar',
         data: []
     }) {
-        let canvas = document.createElement('canvas');
-        canvas.width = 200;
-        canvas.height = 200;
-        canvas.style.border = '1px solid #ccc';
+        // let canvas = document.createElement('canvas');
+        // canvas.width = 200;
+        // canvas.height = 200;
+        // canvas.style.border = '1px solid #ccc';
 
-        document.body.appendChild(canvas);
+        // document.body.appendChild(canvas);
 
-        let context = canvas.getContext('2d');
-        context.fillStyle = '#ff0000';
-        context.fillRect(0, 0, 150, 75);
+        // let context = canvas.getContext('2d');
+        // context.fillStyle = '#ff0000';
+        // context.fillRect(0, 0, 150, 75);
+        let ctx = new Context();
+        ctx.render();
     }
 };
